7cbc375830015a61981b8cad346469997a17c477
suojae <suojae3@gmail.com>
2023-07-27 21:22:26 +0900

    id 추가

--- algorithm/data.swift
+++ algorithm/data.swift
@@ -15,31 +15,31 @@ struct Book {
 //도서관 책 소장 목록
 var library: [String: Book] = [
     
-    "Book1": Book(title: "Book1", author: "Author1", isLend: true, genre: "Essay", sales: 1),
-    "Book2": Book(title: "Book2", author: "Author2", isLend: false, genre: "Essay", sales: 2),
-    "Book3": Book(title: "Book3", author: "Author3", isLend: false, genre: "Essay", sales: 3),
-    "Book4": Book(title: "Book4", author: "Author4", isLend: false, genre: "Essay", sales: 4),
-    "Book5": Book(title: "Book5", author: "Author5", isLend: false, genre: "Fiction", sales: 5),
-    "Book6": Book(title: "Book6", author: "Author6", isLend: false, genre: "Fiction", sales: 6),
-    "Book7": Book(title: "Book7", author: "Author7", isLend: false, genre: "Fiction", sales: 7),
-    "Book8": Book(title: "Book8", author: "Author8", isLend: false, genre: "Non-Fiction", sales: 8),
-    "Book9": Book(title: "Book9", author: "Author9", isLend: false, genre: "Non-Fiction", sales: 9),
-    "Book10": Book(title: "Book10", author: "Author10", isLend: false, genre: "Non-Fiction", sales: 10)
+    "book1": Book(title: "Book1", author: "Author1", isLend: true, genre: "Essay", sales: 1),
+    "book2": Book(title: "Book2", author: "Author2", isLend: false, genre: "Essay", sales: 2),
+    "book3": Book(title: "Book3", author: "Author3", isLend: false, genre: "Essay", sales: 3),
+    "book4": Book(title: "Book4", author: "Author4", isLend: false, genre: "Essay", sales: 4),
+    "book5": Book(title: "Book5", author: "Author5", isLend: false, genre: "Fiction", sales: 5),
+    "book6": Book(title: "Book6", author: "Author6", isLend: false, genre: "Fiction", sales: 6),
+    "book7": Book(title: "Book7", author: "Author7", isLend: false, genre: "Fiction", sales: 7),
+    "book8": Book(title: "Book8", author: "Author8", isLend: false, genre: "Non-Fiction", sales: 8),
+    "book9": Book(title: "Book9", author: "Author9", isLend: false, genre: "Non-Fiction", sales: 9),
+    "book10": Book(title: "Book10", author: "Author10", isLend: false, genre: "Non-Fiction", sales: 10)
     
 ]
 
 //회원정보
 struct Member {
+    let id: Int
     let name: String
     let phoneNumber: String
     var borrowedBooks: [String]
 }
 
 var members: [String: Member] = [
-    "종혁": Member(name: "종혁", phoneNumber: "1", borrowedBooks: ["Book1"]),
-    "주연k": Member(name: "주연k", phoneNumber: "2", borrowedBooks: []),
-    "주연j": Member(name: "주연j", phoneNumber: "3", borrowedBooks: []),
-    "서진": Member(name: "서진", phoneNumber: "4", borrowedBooks: []),
-    "은서": Member(name: "은서", phoneNumber: "5", borrowedBooks: []),
+    "jonhyuck": Member(id: 001, name: "종혁", phoneNumber: "1", borrowedBooks: ["book1"]),
+    "juyeon_k": Member(id: 002, name: "주연k", phoneNumber: "2", borrowedBooks: []),
+    "juyeon_j": Member(id: 003, name: "주연j", phoneNumber: "3", borrowedBooks: []),
+    "seojin": Member(id: 004, name: "서진", phoneNumber: "4", borrowedBooks: []),
+    "eunseo": Member(id: 005, name: "은서", phoneNumber: "5", borrowedBooks: []),
 ]
-
