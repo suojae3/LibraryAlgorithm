9cbaff7605c9103e163cfcd7053e1c6ac0bc24a5
kimseojin <zzin2990@gmail.com>
2023-07-28 17:41:28 +0900

    0728 17:41

--- algorithm/register.swift
+++ algorithm/register.swift
@@ -2,11 +2,12 @@
 import Foundation
 let main = readInput()
 
-struct Register{
-    var member :Int = 0
-    var number: Int = 0
+class Register{
+    var email :String = ""
+    //var number: Int = 0
     var name: String = ""
-    var home: Int = 0
+    var number:String = ""
+    
 
 
     //회원등록 메인화면
@@ -24,7 +25,7 @@ struct Register{
             memberName()
         }
         else if member == 2 {
-            readInput() //메인화면으로 돌아가기
+            readInput()
         }
         else{
             print("잘못입력하셨습니다 다시입력해주세요. ")
@@ -34,38 +35,36 @@ struct Register{
     }
         
         func memberName(){
-            var name: String = ""
+            
             print("회원명을 입력해주세요. ")
-            name = readLine() ?? "";
+            name = readLine() ?? ""
             phoneNumber()
     
         }
         
         func phoneNumber(){
-            var number: Int = 0
             print("전화번호를 입력해주세요. ")
-            number = Int(readLine()!)!
+            number = readLine() ?? ""
+            //number = Int(readLine()!)!
             emailId()
         }
         
         func emailId(){
-            var email: String = ""
             print("이메일을 입력해주세요 . ")
-            email = readLine() ?? "";
+            email = readLine() ?? ""
             memberCkeck()
         }
      
     func memberCkeck(){
-        var check : String = "name" + String(number) + "email"
-        print(check)
+        print("이름: \(name)  전화번호: \(number)  이메일 : \(email)" )
         print("위의 정보가 맞으실까요?")
         print("맞으시면 1번 재입력을 원하시면 2번을 눌러주세요. ")
         var number : Int = 0
         number = Int(readLine() ?? "") ?? 0
         if number == 1 {
+            print("-----------------")
             print("등록 완료 되었습니다. ")
-            //check 의 입력값 배열에 넣기
-            var check:[String]
+            print("-----------------")
         }
         else if number == 2 {
           return memberName()
